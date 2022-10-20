8e1fdc59fd438432dff1ee28518ac3b8e644fca4
Przemek <przemyslaw.falkowski@informationtechconsultants.co.uk>
2022-10-20 16:24:42 +0100

    Utility classes + extensions  added, alert popup, operators to textDidChange event, checking internet connection module implemented

---
+++ Movies/Utility/Extensions/Controller+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Controller+Extension.swift
+//  Movies
+//
+//  Created by Przemek on 25/01/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showAlert(message:String) {
+        let alertViewController = UIAlertController(title:"Message", message:message, preferredStyle: UIAlertController.Style.alert)
+        let alertAction = UIAlertAction(title:"Ok", style: UIAlertAction.Style.cancel, handler: { (alert) in
+            alertViewController.dismiss(animated:true, completion:nil)
+        })
+        alertViewController.addAction(alertAction)
+        self.present(alertViewController, animated:true, completion:nil)
+    }
+}
---
+++ Movies/Utility/Extensions/SearchBar+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  SearchBar+Extension.swift
+//  Movies
+//
+//  Created by Przemek on 25/01/22.
+//
+
+import Foundation
+import Combine
+import UIKit
+
+extension UITextField {
+    var textPublisher: AnyPublisher<String, Never> {
+        NotificationCenter.default
+            .publisher(for: UITextField.textDidChangeNotification, object: self)
+            .compactMap { $0.object as? UITextField } // receiving notifications with objects which are instances of UITextFields
+            .compactMap(\.text) // extracting text and removing optional values (even though the text cannot be nil)
+            .eraseToAnyPublisher()
+    }
+}
