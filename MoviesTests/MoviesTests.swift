83fdfbbb136a0eac8f0e12e7e784203bd5480f59
Przemek <przemyslaw.falkowski@informationtechconsultants.co.uk>
2022-10-20 16:05:32 +0100

    Initial Commit

---
+++ MoviesTests/MoviesTests.swift
@@ -0,0 +1,59 @@
+//
+//  MoviesTests.swift
+//  MoviesTests
+//
+//  Created by Przemek on 24/01/22.
+//
+
+import XCTest
+@testable import Movies
+
+class MoviesTests: XCTestCase {
+    
+    var viewModel:MoviesViewModel!
+    var respository:MovieRepository!
+    var networkManager:MockNetworkManager!
+    var movieCoreDataRepo:MockMovieCoreDataRepository!
+    
+    override func setUpWithError() throws {
+        
+        networkManager = MockNetworkManager()
+        movieCoreDataRepo = MockMovieCoreDataRepository()
+        
+        respository = MovieRepository(networkManager: networkManager, movieCoreDataRepo: movieCoreDataRepo)
+        viewModel = MoviesViewModel(repository: respository)
+    }
+
+    override func tearDownWithError() throws {
+        viewModel = nil
+    }
+    
+    func testSearchMovie_success() {
+        
+        let movieApiRquest = ApiRequest(baseUrl: EndPoint.baseUrl, path: "movie_success", params: [:])
+               
+        viewModel.searchMovies(keyword: "a", apiRequest: movieApiRquest)
+        
+        XCTAssertEqual(viewModel.movieCount, 20)
+        XCTAssertEqual(viewModel.movies.count, 20)
+        
+        XCTAssertEqual(viewModel.movies[0].isFav, false)
+
+        XCTAssertEqual(viewModel.movies[1].isFav, true)
+
+    }
+    
+    func testSearchMovieForFavouriteMovies_success() {
+        
+        let movieApiRquest = ApiRequest(baseUrl: EndPoint.baseUrl, path: "movie_success", params: [:])
+               
+        viewModel.searchMovies(keyword: "", apiRequest: movieApiRquest)
+        
+        XCTAssertEqual(viewModel.movieCount, 1)
+        XCTAssertEqual(viewModel.movies.count, 1)
+        
+        XCTAssertEqual(viewModel.movies[0].isFav, true)
+
+    }
+    
+}
---
+++ MoviesUITests/MoviesUITestsLaunchTests.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesUITestsLaunchTests.swift
+//  MoviesUITests
+//
+//  Created by Przemek on 24/01/22.
+//
+
+import XCTest
+
+class MoviesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+  
+}
