d083e1be9f800f878f9c4db63e55ba7630df8388
Przemek <przemyslaw.falkowski@informationtechconsultants.co.uk>
2022-10-20 16:19:25 +0100

    MovieDetails screen added, MovieDetailsViewController and MovieDetailsViewModel implemented

---
+++ Movies/Modules/MovieDetails/MovieDetailsViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDetailsViewModel.swift
+//  Movies
+//
+//  Created by Przemek on 25/01/22.
+//
+
+import Foundation
+
+class MovieDetailsViewModel {
+    
+    let movie:Movie
+    
+    init(movie:Movie) {
+        self.movie = movie
+    }
+}
