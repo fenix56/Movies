c5917002bad8606e7580d42325c4ea369c904c01
Przemek <przemyslaw.falkowski@informationtechconsultants.co.uk>
2022-10-20 16:16:33 +0100

    movies searching feature added, table view cell connected, core data [WIP]

---
+++ Movies/Modules/Search/MovieTableViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  MovieTableViewCell.swift
+//  Movies
+//
+//  Created by Przemek on 25/01/22.
+//
+
+import UIKit
+import Kingfisher
+
+protocol MovieCellDelegate: AnyObject {
+    func favAction(isSelected:Bool, index:Int)
+}
+
+class MovieTableViewCell: UITableViewCell {
+    @IBOutlet weak var overViewLbl: UILabel!
+    @IBOutlet weak var favButton: UIButton!
+    @IBOutlet weak var reviewsCountLbl: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    weak var delegate:MovieCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    override func prepareForReuse() {
+        posterImageView.image = nil
+        titleLbl.text = ""
+        overViewLbl.text = ""
+        reviewsCountLbl.text = ""
+        favButton.setImage(UIImage(named:"favourite_unselected"), for: .normal)
+
+    }
+
+    func setData(movie:Movie, index:Int) {
+        titleLbl.text = movie.title
+        reviewsCountLbl.text = "Ratings : \(movie.reviews)"
+        favButton.tag = index
+        let url = URL(string:"\(EndPoint.imagesBaseUrl)\(movie.poster)")
+        posterImageView.kf.setImage(with: url)
+        
+        overViewLbl.text = movie.overView
+
+        let imageName = movie.isFav ? "favourite_selected" : "favourite_unselected"
+        favButton.setImage(UIImage(named: imageName), for: .normal)
+        
+        favButton.isSelected = movie.isFav
+
+    }
+    
+    @IBAction func favouriteButtonTapped(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        let imageName = sender.isSelected ? "favourite_selected" : "favourite_unselected"
+        sender.setImage(UIImage(named: imageName), for: .normal)
+        
+        delegate?.favAction(isSelected:sender.isSelected, index: sender.tag)
+    }
+    
+}
